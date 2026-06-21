133a9364422eae53f4a9a1b028735e2a0345ed1c
Aadi <krithn.aadityan@gmail.com>
2026-06-21 18:50:45 +0530

    Add files via upload

---
+++ Java FSE/Deepskilling/Engineering Concepts/Design Pattern & Principles/Exercise5/DecoratorPattern.java
@@ -0,0 +1,55 @@
+interface Notifier {
+    void send(String message);
+}
+
+class EmailNotifier implements Notifier {
+    @Override
+    public void send(String message) {
+        System.out.println("Sending Email: " + message);
+    }
+}
+
+abstract class NotifierDecorator implements Notifier {
+    protected Notifier wrappedNotifier;
+
+    public NotifierDecorator(Notifier notifier) {
+        this.wrappedNotifier = notifier;
+    }
+
+    @Override
+    public void send(String message) {
+        wrappedNotifier.send(message);
+    }
+}
+
+class SMSNotifierDecorator extends NotifierDecorator {
+    public SMSNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+
+    @Override
+    public void send(String message) {
+        super.send(message);
+        sendSMS(message);
+    }
+
+    private void sendSMS(String message) {
+        System.out.println("Sending SMS text: " + message);
+    }
+}
+
+class SlackNotifierDecorator extends NotifierDecorator {
+    public SlackNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+
+    @Override
+    public void send(String message) {
+        super.send(message);
+        sendSlackMessage(message);
+    }
+
+    private void sendSlackMessage(String message) {
+        System.out.println("Posting to Slack Channel: " + message);
+    }
+}
